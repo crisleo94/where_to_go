97b9e027ceb2bccae492c0873d5d9cf0145faeac
crisleo94 <cristhian.reinoso@live.com>
2022-02-03 12:47:53 -0500

    update PlaceDetails component

--- components/PlaceDetails.jsx
+++ components/PlaceDetails.jsx
@@ -1,5 +1,11 @@
-
+import { usePlace } from "../hooks/usePlace"
+import { useEffect } from "react"
 
 export default function PlaceDetails({ place }) {
+  const {place: placeLocation} = usePlace()
+  useEffect(() => {
+    console.log(placeLocation)
+  }, [])
+
   return <div>{place.name} - <b>{place.location}</b></div>
 }
